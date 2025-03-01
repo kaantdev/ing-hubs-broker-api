3ebfb522e689a2847e0c694a50d8e2ccb6670588
kaantdev <kaant@outlook.com>
2025-03-01 22:17:16 +0300

    initial commit

---
+++ src/main/java/com/ing/hubs/broker_api/BrokerApiApplication.java
@@ -0,0 +1,13 @@
+package com.ing.hubs.broker_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrokerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrokerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ing/hubs/broker_api/BrokerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ing.hubs.broker_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrokerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
